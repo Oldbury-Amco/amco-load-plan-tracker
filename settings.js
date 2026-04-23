1124c0b4d47e7af760cb25c04adf816b22c370ae
Oldbury-Amco <alansamuelhall@gmail.com>
2026-04-23 20:21:19 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  supabaseUrl: "https://rotvlxhlfphpfyfpboqz.supabase.co",
+  supabaseUrl: "https://rotvlxhflpphfyfpboqz.supabase.co",
   supabaseAnonKey: "sb_publishable_PV2Clhs-T4X75OseYCJCWQ_1zTQMQW8",
   trackerId: "oldbury-main",
   defaultKennsPerLoad: 11
